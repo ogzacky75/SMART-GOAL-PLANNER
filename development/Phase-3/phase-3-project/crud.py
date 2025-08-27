1b6028ca0f79362607a0862573b49c22ff460054
ogzacky75 <ogzacky75@gmail.com>
2025-08-27 14:59:56 +0300

    adding canvas template

---
+++ development/Phase-3/phase-3-project/crud.py
@@ -0,0 +1,10 @@
+import click
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+#CREATE
+@click.command()
+def create_user(session, username, role):
+	pass
+#READ
+#UPDATE
+#DELETE
