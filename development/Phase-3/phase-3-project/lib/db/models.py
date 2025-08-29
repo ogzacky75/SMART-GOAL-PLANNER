0b92b5698a8b3cade8d3e9a1b34a23858f096681
ogzacky75 <ogzacky75@gmail.com>
2025-08-29 12:01:21 +0300

    fixing relationships

--- development/Phase-3/phase-3-project/lib/db/models.py
+++ development/Phase-3/phase-3-project/lib/db/models.py
@@ -8,6 +8,12 @@ class Users(Base):
 	username = Column(String, unique=True)
 	role = Column(String)
 	created_at = Column(DateTime, default=func.now())
+
+	skills = relationship("Skills", back_populates="creator", cascade="all, delete-orphan")
+	progress = relationship("UserProgress", back_populates="user", cascade="all, delete-orphan")
+	followers = relationship("Follows", foreign_keys="[Follows.followed_id]", back_populates="followed")
+	following = relationship("Follows", foreign_keys="[Follows.follower_id]", back_populates="follower")
+
 class skills(Base):
 	__tablename__ = 'skills'
 	id = Column(Integer, primary_key=True)
@@ -18,6 +24,10 @@ class skills(Base):
 	difficulty = Column(String)
 	created_at = Column(DateTime, default=func.now())
 
+	creator = relationship("Users", back_populates="skills")
+	progress = relationship("UserProgress", back_populates="skill", cascade="all, delete-orphan")
+
+
 
 class User_progress(Base):
 	__tablename__ = 'user_progress'
@@ -26,6 +36,11 @@ class User_progress(Base):
 	skill_id = Column(Integer, ForeignKey('skills.id'))
 	status = Column(String)
 
+	user = relationship("Users", back_populates="progress")
+	skill = relationship("Skills", back_populates="progress")
+
+
+
 class follows(Base):
 	__tablename__ = 'follows'
 	id = Column(Integer, primary_key=True)
@@ -33,7 +48,11 @@ class follows(Base):
 	followed_id = Column(Integer, ForeignKey('users.id'))
 	created_at = Column(DateTime, default=func.now())
 
+	follower = relationship("Users", foreign_keys=[follower_id], back_populates="following")
+	followed = relationship("Users", foreign_keys=[followed_id], back_populates="followers")
+
 
 DATABASE_URL = "sqlite:///micro.db"
 engine = create_engine(DATABASE_URL, echo=True)
-Base.metadata.create_all(engine)	
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)	
